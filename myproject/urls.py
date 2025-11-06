da7fcef622657c158c86074b6a3d5398b6906dd5
sowmisasi0609 <sowmisasi0609@gmail.com>
2025-11-06 13:35:03 +0530

    added

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("myapp.urls")),
+    path('dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('', lambda request: redirect('student_dashboard')),
 ]
+
 
